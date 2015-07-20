1ee24560ec2c2c48fdb44ffa9943fe65e5e34b52
HurbDurb <daanhenkek@gmail.com>
2015-07-20 21:31:55 +0200

    fixed the getSect function

--- libs/libConfig.py
+++ libs/libConfig.py
@@ -9,18 +9,8 @@ def getOption(file, section, option, default):
         return opt;
 
 def getSection(file, section):
-    if os.path.isfile(file) != True:
-        file = open(file, "w+");
-    Config = ConfigParser.ConfigParser();
-    Config.read(file);
-    sect = {}
-    options = Config.options(section);
-    for option in options:
-        try:
-            sect[option] = Config.get(section, option);
-        except:
-            sect[option] = None;
-    return sect;
+   sect = getFile(file)[section];
+   return sect;
 
 def getFile(file):
     if os.path.isfile(file) != True:
